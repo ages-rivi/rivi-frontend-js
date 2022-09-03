2b57be91be92094a97be27accea554ca6510be06
guilherme <guilherme.silva01@edu.pucrs.br>
2022-09-03 15:55:58 -0300

    feat: adicionado cabeçalho

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,9 +1,11 @@
 import { ChakraProvider } from '@chakra-ui/react';
+import Navbar from '@components/Navbar';
 import type { AppProps } from 'next/app';
 
 function MyApp({ Component, pageProps }: AppProps): React.ReactNode {
   return (
     <ChakraProvider>
+      <Navbar />
       <Component {...pageProps} />
     </ChakraProvider>
   );
