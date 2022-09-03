7c95f8bf8e78181dc58d66628011c3cb52413d70
guilherme <guilherme.silva01@edu.pucrs.br>
2022-09-03 12:03:12 -0300

    feat: eslint, prettier, commit e ts configurados

---
+++ src/pages/_app.tsx
@@ -0,0 +1,8 @@
+import type { AppProps } from 'next/app';
+import '../styles/globals.css';
+
+function MyApp({ Component, pageProps }: AppProps): React.ReactNode {
+  return <Component {...pageProps} />;
+}
+
+export default MyApp;
