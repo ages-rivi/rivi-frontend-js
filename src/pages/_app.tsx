475965cb6b129f24653b58fcf6696593183aec92
guilherme <guilherme.silva01@edu.pucrs.br>
2022-09-03 12:36:31 -0300

    fix: babel core removido

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,5 +1,4 @@
 import type { AppProps } from 'next/app';
-import '../styles/globals.css';
 
 function MyApp({ Component, pageProps }: AppProps): React.ReactNode {
   return <Component {...pageProps} />;
