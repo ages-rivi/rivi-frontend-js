5a9724daf3c496497002f0d20d2cc6995bb3b050
guigaribalde <guilherme@creatusdev.com>
2022-09-05 10:56:22 -0300

    feat: visualização de footer e nav em rotas padrão

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,12 +1,18 @@
-import { ChakraProvider } from '@chakra-ui/react';
+import { ChakraProvider, Flex } from '@chakra-ui/react';
+import Footer from '@components/Footer';
 import Navbar from '@components/Navbar';
 import type { AppProps } from 'next/app';
+import { useRouter } from 'next/router';
 
 function MyApp({ Component, pageProps }: AppProps): React.ReactNode {
+  const router = useRouter();
   return (
     <ChakraProvider>
-      <Navbar />
-      <Component {...pageProps} />
+      <Flex direction="column" h="100vh">
+        {!router.pathname.includes('/auth') && <Navbar />}
+        <Component {...pageProps} />
+        {!router.pathname.includes('/auth') && <Footer />}
+      </Flex>
     </ChakraProvider>
   );
 }
