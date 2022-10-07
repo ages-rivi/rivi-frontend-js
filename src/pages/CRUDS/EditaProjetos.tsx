5d53e2bfe482da8375d0acf34cce099ec3e2230a
Eduardo Aguiar de Godoi <eduadg@hotmail.com>
2022-10-07 04:38:09 -0300

    feat: CRUD pagina EditaProjetos

--- src/pages/CRUDS/EditaProjetos.tsx
+++ src/pages/CRUDS/EditaProjetos.tsx
@@ -1,106 +1,113 @@
-/* import { Flex, Text } from '@chakra-ui/react';
-// import ProjectItem from '@components/ProjectCard';
-import DataTable from '@components/DataTable';
+import {
+  Box,
+  Button,
+  ChakraProvider,
+  Flex,
+  Text,
+  Textarea,
+} from '@chakra-ui/react';
 
 function EditaProjetos(): JSX.Element {
   return (
-    <Flex direction="column">
+    <Flex direction="column" p="5">
       <Flex direction="column" maxW="1330px" w="full" margin="auto" gap="3">
-        <Text textAlign="center" fontWeight="medium" fontSize="5xl">
-          Edita Projetos
+        <Text textAlign="Left" fontWeight="medium" fontSize="3xl">
+          Projeto dos Alunos
         </Text>
+        <ChakraProvider>
+          <Box p={4}>
+            <Text
+              textAlign="left"
+              mt="6"
+              mb="5"
+              fontWeight="inter"
+              gap="5"
+              fontSize="1xl"
+            >
+              Título
+            </Text>
+
+            <Textarea
+              mt="-5"
+              placeholder="Ex: Relações Interpessoais"
+              textAlign="left"
+            />
+            <Text
+              textAlign="left"
+              mt="6"
+              mb="5"
+              fontWeight="inter"
+              gap="5"
+              fontSize="1xl"
+            >
+              Descrição
+            </Text>
+
+            <Textarea
+              mt="-5"
+              placeholder="Ex: Descrição do projeto"
+              textAlign="left"
+            />
+            <Text
+              textAlign="left"
+              mt="6"
+              mb="5"
+              fontWeight="inter"
+              gap="5"
+              fontSize="1xl"
+            >
+              Temáticas
+            </Text>
+
+            <Textarea
+              mt="-5"
+              placeholder="Ex: autocompaixão"
+              textAlign="left"
+            />
+            <Text
+              textAlign="left"
+              mt="6"
+              mb="5"
+              fontWeight="inter"
+              gap="5"
+              fontSize="1xl"
+            >
+              Aluno
+            </Text>
+
+            <Textarea mt="-5" placeholder="Ex: John Doe" textAlign="left" />
+            <Text
+              textAlign="left"
+              mt="6"
+              mb="5"
+              fontWeight="inter"
+              gap="5"
+              fontSize="1xl"
+            >
+              Afiliações
+            </Text>
+
+            <Textarea mt="-5" placeholder="PUCRS" textAlign="left" />
+            <Flex
+              direction={{ base: 'column', md: 'row' }}
+              mt="14"
+              gap={{ base: '2', md: '6' }}
+              w={{ base: 'full', md: 'sm' }}
+            >
+              <Button colorScheme="teal" w={{ base: 'full', md: '' }}>
+                Salvar
+              </Button>
+              <Button
+                colorScheme="black"
+                variant="outline"
+                w={{ base: 'full', md: '' }}
+              >
+                Cancelar
+              </Button>
+            </Flex>
+          </Box>
+        </ChakraProvider>
       </Flex>
-      <DataTable />
-    </Flex>
-  );
-}
-
-export default EditaProjetos;
-*/
-
-import { CheckCircleIcon, TimeIcon } from '@chakra-ui/icons';
-import { Flex, Text } from '@chakra-ui/react';
-import { useEffect, useRef, useState } from 'react';
-import { ChakraUITable } from 'react-chakra-ui-table';
-
-// Example Table
-function TodoListTable() {
-  const columns = [
-    {
-      Header: '#',
-      Cell: ({ row }) => {
-        return <Text>{row.index + 1}</Text>;
-      },
-    },
-    {
-      Header: 'Name',
-      accessor: 'name',
-    },
-    {
-      Header: 'Title',
-      accessor: 'title',
-    },
-    {
-      Header: 'Completed',
-      accessor: 'completed',
-      Cell: ({ value }) => {
-        return value ? (
-          <CheckCircleIcon boxSize={5} color="green.400" />
-        ) : (
-          <TimeIcon boxSize={5} color="orange.400" />
-        );
-      },
-    },
-  ];
-
-  const [data, setData] = useState(null);
-
-  const loadData = useRef();
-
-  loadData.current = async () => {
-    const urls = [
-      'https://jsonplaceholder.typicode.com/users',
-      'https://jsonplaceholder.typicode.com/todos',
-    ];
-
-    try {
-      const result = await Promise.all(
-        urls.map((url) => {
-          return fetch(url).then((r) => {
-            return r.json();
-          });
-        })
-      );
-
-      if (result.length === 2) {
-        // index 0 is user
-        // index 1 is todo
-        const todoList = result[1].map((todo) => {
-          todo.user = result[0].find((i) => {
-            return i.id === todo.userId;
-          });
-          todo.name = todo.user?.name;
-          return todo;
-        });
-
-        setData(todoList);
-      }
-    } catch (error) {
-      console.log(error);
-    }
-  };
-
-  useEffect(() => {
-    loadData.current();
-  }, []);
-
-  return data && <ChakraUITable columns={columns} data={data} />;
-}
-
-function EditaProjetos(): JSX.Element {
-  return (
-    <Flex p={6} direction="column">
-      <TodoListTable />
     </Flex>
   );
 }
