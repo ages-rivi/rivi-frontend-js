cfddd1ed928024deeb1cec35e22030bc21da9bfa
Eduardo Godoi <eduadg@hotmail.com>
2022-10-07 22:36:13 -0300

    fix: desenvolvida a pagina EditaProjetos

--- src/pages/CRUDS/EditaProjetos.tsx
+++ src/pages/CRUDS/EditaProjetos.tsx
@@ -1,115 +1,81 @@
 import {
-  Box,
   Button,
-  ChakraProvider,
   Flex,
+  FormControl,
+  FormLabel,
+  Input,
   Text,
   Textarea,
 } from '@chakra-ui/react';
 
-function EditaProjetos(): JSX.Element {
+export default function EditaProjetos(): JSX.Element {
+  const handleSubmit = (e: React.FormEvent<HTMLFormElement>): void => {
+    e.preventDefault();
+    const data = new FormData(e.currentTarget);
+
+    // eslint-disable-next-line
+    // @ts-ignore
+    // eslint-disable-next-line
+    for (const [key, value] of data.entries()) {
+      console.log(`${key}: ${value}`);
+    }
+  };
+
   return (
     <Flex direction="column" p="5">
       <Flex direction="column" maxW="1330px" w="full" margin="auto" gap="3">
-        <Text textAlign="Left" fontWeight="medium" fontSize="3xl">
+        <Text fontWeight="medium" fontSize="3xl">
           Projeto dos Alunos
         </Text>
-        <ChakraProvider>
-          <Box p={4}>
-            <Text
-              textAlign="left"
-              mt="6"
-              mb="5"
-              fontWeight="inter"
-              gap="5"
-              fontSize="1xl"
-            >
-              Título
-            </Text>
-
-            <Textarea
-              mt="-5"
-              placeholder="Ex: Relações Interpessoais"
-              textAlign="left"
-            />
-            <Text
-              textAlign="left"
-              mt="6"
-              mb="5"
-              fontWeight="inter"
-              gap="5"
-              fontSize="1xl"
-            >
-              Descrição
-            </Text>
-
-            <Textarea
-              mt="-5"
-              placeholder="Ex: Descrição do projeto"
-              textAlign="left"
-            />
-            <Text
-              textAlign="left"
-              mt="6"
-              mb="5"
-              fontWeight="inter"
-              gap="5"
-              fontSize="1xl"
-            >
-              Temáticas
-            </Text>
-
-            <Textarea
-              mt="-5"
-              placeholder="Ex: autocompaixão"
-              textAlign="left"
-            />
-            <Text
-              textAlign="left"
-              mt="6"
-              mb="5"
-              fontWeight="inter"
-              gap="5"
-              fontSize="1xl"
-            >
-              Aluno
-            </Text>
-
-            <Textarea mt="-5" placeholder="Ex: John Doe" textAlign="left" />
-            <Text
-              textAlign="left"
-              mt="6"
-              mb="5"
-              fontWeight="inter"
-              gap="5"
-              fontSize="1xl"
-            >
-              Afiliações
-            </Text>
-
-            <Textarea mt="-5" placeholder="PUCRS" textAlign="left" />
+        <form onSubmit={handleSubmit}>
+          <Flex direction="column" p={4} gap="3">
+            <FormControl>
+              <FormLabel>Título</FormLabel>
+              <Input name="title" placeholder="Ex: Relações Interpessoais" />
+            </FormControl>
+            <FormControl>
+              <FormLabel>Temáticas</FormLabel>
+              <Input name="themes" placeholder="Ex: autocompaixão" />
+            </FormControl>
+            <FormControl>
+              <FormLabel>Aluno</FormLabel>
+              <Input name="student" placeholder="Ex: John Doe" />
+            </FormControl>
+            <FormControl>
+              <FormLabel>Afiliações</FormLabel>
+              <Input name="afiliations" placeholder="PUCRS" />
+            </FormControl>
+            <FormControl>
+              <FormLabel>Descrição</FormLabel>
+              <Textarea
+                name="description"
+                placeholder="Ex: Descrição do projeto"
+              />
+            </FormControl>
             <Flex
               direction={{ base: 'column', md: 'row' }}
               mt="14"
               gap={{ base: '2', md: '6' }}
               w={{ base: 'full', md: 'sm' }}
             >
-              <Button colorScheme="teal" w={{ base: 'full', md: '' }}>
+              <Button
+                colorScheme="teal"
+                w={{ base: 'full', md: '' }}
+                type="submit"
+              >
                 Salvar
               </Button>
               <Button
-                colorScheme="black"
+                colorScheme="gray"
                 variant="outline"
                 w={{ base: 'full', md: '' }}
               >
                 Cancelar
               </Button>
             </Flex>
-          </Box>
-        </ChakraProvider>
+          </Flex>
+        </form>
       </Flex>
     </Flex>
   );
 }
-
-export default EditaProjetos;
