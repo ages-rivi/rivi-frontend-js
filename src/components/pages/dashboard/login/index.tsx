8d06b03194d03cf2aa4d9a6dcd8042232a690b0a
lucashleal <lucas.hanauer@outlook.com>
2022-11-11 22:38:04 -0300

    add arquivos da pagina de login

---
+++ src/components/pages/dashboard/login/index.tsx
@@ -0,0 +1,12 @@
+import { Flex, Text } from '@chakra-ui/react';
+// import LoginPage from ;
+
+export default function Login() {
+  return (
+    <Flex p="5" direction="column" mt="5" align="center" gap="3">
+      <Text textAlign="center" fontWeight="medium" fontSize="4xl">
+        Artigos
+      </Text>
+    </Flex>
+  );
+}
---
+++ src/pages/admin/login/login.tsx
@@ -0,0 +1,5 @@
+import LoginPage from 'src/components/pages/dashboard/login';
+
+export default function Login() {
+  return <LoginPage />;
+}
