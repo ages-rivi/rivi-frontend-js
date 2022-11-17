968d8af7eb4d747846f56cbb6aa90d759fc95396
lucashleal <lucas.hanauer@outlook.com>
2022-11-17 16:46:35 -0300

    Add lista projetos na pagina de admin

--- old_src/lib/projetos.ts
+++ old_src/lib/projetos.ts
@@ -1,13 +1,13 @@
 import api from 'old_src/lib/api';
 
 async function getAllProjects() {
-  const res = await api.get('/projetos');
+  const res = await api.get('/projeto');
   const projects = await res.data;
   return projects;
 }
 
 async function getProjectById(id: String) {
-  const res = await api.get('/projetos/' + id);
+  const res = await api.get('/projeto/' + id);
   const projects = await res.data;
   return projects;
 }
